91812336d8d091c14fddd66302b3b0961a7a9573
Neyamat Ullah <neyamat7.ullah@gmail.com>
2025-02-15 09:09:08 +0600

    find lowest number

---
+++ part-2-practice-task.js
@@ -0,0 +1,15 @@
+//task -1
+// Find the lowest number in the array below:
+// const heights = [30, 154, 20, 167, 190, 120, 165, 137];
+
+// function findLowestNumber(numbers) {
+//   let lowestNumber = numbers[0];
+//   for (const number of numbers) {
+//     if (number < lowestNumber) {
+//       lowestNumber = number;
+//     }
+//   }
+//   return lowestNumber;
+// }
+
+// console.log(findLowestNumber(heights));
